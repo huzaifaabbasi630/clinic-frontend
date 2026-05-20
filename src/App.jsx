9816a1c740e47b76f2d4447bc5effb17fe46731d
huzaifaabbasi630 <huzaifaabbasi09123@gmail.com>
2026-05-20 13:18:50 +0500

    remove errors

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
-import { AuthProvider } from './context/AuthContext';
+import { AuthProvider, useAuth } from './context/AuthContext';
 
 // Layouts
 import DashboardLayout from './layouts/DashboardLayout';
